1206f17adf02be5b5914ad5abdeda93101699702
Kamal Nayan Upadhyay <93386296+88kamal@users.noreply.github.com>
2025-09-29 09:36:49 +0530

    Update AllProductsPage.jsx

--- src/pages/AllProductsPage.jsx
+++ src/pages/AllProductsPage.jsx
@@ -454,7 +454,7 @@ const AllProducts = () => {
                   <Typography className="text-gray-700 text-sm mb-4">{product.description}</Typography>
                   
                   <div className="flex flex-col gap-2">
-                    <a href="tel:+918888888888">
+                    <a href="tel:+919311859997">
                       <Button fullWidth className="bg-blue-600 hover:bg-blue-700 text-white text-base font-semibold">
                         Book Now
                       </Button>
